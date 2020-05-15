86b881370eb756ce97baf2757619085e7b1667bf
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2020-05-14 19:20:37 -0500

    api de descarga

--- index.js
+++ index.js
@@ -21,24 +21,49 @@ app.get('/help', function(req, res){
         help:[
             '/download/{id}?resolution={number}',
             'download/audio/{id}',
-            '/view/{id}?type={audio|video}',
+            '/save/{id}?type={audio|video}',
             '/info/{id}'
         ]
     });
 });
+/**
+ * res.params.id : numeric (11 digits)
+ * res.query.resolution : numeric(144|240|360|480|720|1080)
+ */
 app.get('/download/:id', function(req, res) {
+    let resolution= '480';
+    if( req.query.resolution ){
+        resolution =  req.query.resolution;
+        let validResolution = ['144','240','360','480','720','1080'];
+        let isValidRs = false;
+        validResolution.forEach( rs => {
+            if(rs == resolution){
+                isValidRs = true;
+            }
+        });
+        resolution = isValidRs ? req.query.resolution : '480';
+    }else{
+        resolution = '480';
+    }
+    // Will be called when the download starts.
+    let extension = 'mp4'
+    let file ='';
+    let size ='';
+    let formats= [];
     if(req.params.id.length!= 11){
         return res.json({ 
             'message':'Error the id must contain 11 characters 😫 id',
             'error':'Internal Error'
         });
     }
-    let resolution = (req.query.resolution)? req.query.resolution:'720' ;
-    // Will be called when the download starts.
-    let extension = 'mp4'
-    let file ='';
-    let size ='';
-    let formats= [];
+    if(fs.existsSync(`${__dirname}/public/video/${req.params.id}_${resolution}.mp4`)){
+        res.send({ 
+            'message': 'downloaded', 
+            'path': `view/${req.params.id}?resolution=${resolution}&type=video`,
+            'size':size,
+            'file':file
+        });
+    }
     youtubedl.getInfo(`http://www.youtube.com/watch?v=${req.params.id}`, function getInfo(err, info) {
         if (err) {
             return res.json({
@@ -50,25 +75,25 @@ app.get('/download/:id', function(req, res) {
         let crinfo = formats.filter( info => info.resolution.includes(resolution) && info.extension=='mp4' ).pop();
         let video = youtubedl(`https://www.youtube.com/watch?v=${req.params.id}`,[`--format=${crinfo.itag > 0?crinfo.itag:18}`],{ cwd: __dirname });
         video.on('info', function(info) {
-            // console.log('Download started');
-            // console.log('filename: ' + info._filename);
-            // console.log('size: ' + info.size);
+            console.log('Download started');
+            console.log('filename: ' + info._filename);
+            console.log('size: ' + info.size);
             file = info._filename;
             extension = info._filename.split('.')[1];
             size = info.size;
         });
         video.on('end', () => {
-            // console.log('Disponible');
+            console.log('Disponible');
             res.send({ 
                 'message': 'downloaded', 
-                'path': `view/${req.params.id}?resolution=${resolution}`,
+                'path': `view/${req.params.id}?resolution=${resolution}&type=video`,
                 'size':size,
                 'file':file
             });
         }).pipe(fs.createWriteStream(`public/video/${req.params.id}_${resolution}.${extension}`));
     });
 });
-app.get('/view/:id', function(req, res) {
+app.get('/save/:id', function(req, res) {
     let resolution = req.query.resolution;
     if(req.params.id.length!= 11){
         return res.json({ 
@@ -77,20 +102,10 @@ app.get('/view/:id', function(req, res) {
         });
     }
     if(req.query.type=="audio"){
-        return res.send(`
-            <audio controls>
-                <source src="/audio/${req.params.id}.mp3" type="audio/mp3">
-                Your browser does not support the audio element.
-            </audio>
-        `);
+        res.download(`${__dirname}/public/audio/${req.params.id}.mp3`);
     }
     if(req.query.type=="video"){
-        return res.send(`
-            <video controls>
-                <source src="/video/${req.params.id}_${resolution}.mp4" type="video/mp4">
-                Your browser does not support the video tag.
-            </video>
-        `);
+        return res.download(`${__dirname}/public/video/${req.params.id}_${resolution}.mp4`);
     }
     return res.json({ 
         'message':'Error can not find type on your request 💀',
@@ -120,12 +135,18 @@ app.get('/download/audio/:id', function(req, res) {
             'error':'Internal Error'
         });
     }
+    if(fs.existsSync(`${__dirname}/public/audio/${id}.mp3`)){
+        return res.json({
+            'message':'Downloaded',
+            'path':`/view/${id}?type=audio`,
+        });
+    }
     youtubedl.exec(`http://www.youtube.com/watch?v=${req.params.id}`, ['-x', '--audio-format', 'mp3','-o' ,`public/audio/${id}.mp3`],{}, function exec(err, output) {
         if (err) {
             if(fs.existsSync(`${__dirname}/public/audio/${id}.mp3`)){
                 return res.json({
                     'message':'Downloaded',
-                    'path':`/view/audio/${id}.mp3`,
+                    'path':`/view/${id}?type=audio`,
                 });
             }
             return res.json({ 
