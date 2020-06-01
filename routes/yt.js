2752c66d22125db738642b4d8059733357245687
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2020-06-01 01:30:53 -0500

    fixing

--- routes/yt.js
+++ routes/yt.js
@@ -292,56 +292,52 @@ router.get('/download/audio/:id', function(req, res) {
     }else{
         youtubedl.exec(`http://www.youtube.com/watch?v=${id}`, ['-x', '-format','mp3','-o' ,`public/audio/${id}.mp3`],{}, function exec(noErr, output) {
             if(noErr){
-                console.log(noErr);
-            }else{
-                if(output){
-                    console.log(output);
-                }
+                
+                youtubedl.getInfo(`http://www.youtube.com/watch?v=${id}`, function getInfo(err, info) {
+                    console.log(err);
+                    if (err) {
+                        res.json({
+                            'message':'Error when try get info of audio 💀 id:'+id,
+                            'error':'Internal Error'
+                        });
+                    }else{
+                        let song = {
+                            id:id,
+                            title: info.title?info.title:'Unknown',
+                            artist: info.artist?info.artist:'Unknown',
+                            extension:'mp3',
+                            duration: info.duration,
+                            path:`/static/audio/${id}.mp3`,
+                            pathDownload:`/save/${id}?type=audio`,
+                            imagePath:info.thumbnails[0].url    
+                        };
+                        Song.create(song, function(err, song) {
+                            if(err) {
+                                res.json({
+                                    'error' : err
+                                });
+                            }else{
+                                Song.get({}, function(err, songs) {
+                                    if(err) {
+                                        res.json({
+                                            error: err
+                                        })
+                                    }else{
+                                        res.json({
+                                            'song':song,
+                                            'songs':songs,
+                                            'message':'On Server 😁😁😁',
+                                            'path':`/save/${id}?type=audio`,
+                                        });
+                                    }
+                                }); 
+                            }
+                        });
+                    }
+                });
             }
-            youtubedl.getInfo(`http://www.youtube.com/watch?v=${id}`, function getInfo(err, info) {
-                console.log(err);
-                if (err) {
-                    res.json({
-                        'message':'Error when try get info of audio 💀 id:'+id,
-                        'error':'Internal Error'
-                    });
-                }else{
-                    let song = {
-                        id:id,
-                        title: info.title?info.title:'Unknown',
-                        artist: info.artist?info.artist:'Unknown',
-                        extension:'mp3',
-                        duration: info.duration,
-                        path:`/static/audio/${id}.mp3`,
-                        pathDownload:`/save/${id}?type=audio`,
-                        imagePath:info.thumbnails[0].url    
-                    };
-                    Song.create(song, function(err, song) {
-                        if(err) {
-                            res.json({
-                                'error' : err
-                            });
-                        }else{
-                            Song.get({}, function(err, songs) {
-                                if(err) {
-                                    res.json({
-                                        error: err
-                                    })
-                                }else{
-                                    res.json({
-                                        'song':song,
-                                        'songs':songs,
-                                        'message':'On Server 😁😁😁',
-                                        'path':`/save/${id}?type=audio`,
-                                    });
-                                }
-                            }); 
-                        }
-                    });
-                }
-            });
-        
         });    
+            
     }
 });
 module.exports = router;
